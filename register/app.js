618fd8cea6d046386dac4a32aa122ddd54830423
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-06-06 23:45:37 +0430

    avoid post with blank inputs

--- register/app.js
+++ register/app.js
@@ -6,35 +6,41 @@ const submitBtn = document.querySelector(".submit-btn");
 async function sendDatas(e) {
   e.preventDefault();
 
-  let newUser = {
-    id: 1,
-    name: nameInput.value,
-    lname: lastNameInput.value,
-    password: passwordInput.value,
-  };
+  if (
+    nameInput.value !== "" &&
+    lastNameInput.value !== "" &&
+    passwordInput.value !== ""
+  ) {
+    
+    let newUser = {
+      id: 1,
+      name: nameInput.value,
+      lname: lastNameInput.value,
+      password: passwordInput.value,
+    };
 
-  await fetch(
-    "https://login-page1-1ccde-default-rtdb.firebaseio.com/user.json",
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(newUser),
-    }
-  )
-    .then((response) => response)
-    .then((data) => {
-      if (data.status === 200) {
-        resetInputs();
+    await fetch(
+      "https://login-page1-1ccde-default-rtdb.firebaseio.com/user.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(newUser),
       }
-    })
-    .catch((err) => {
-      console.log(err);
-      alert("Please turn you vpn on :)))");
-    });
+    )
+      .then((data) => {
+        if (data.status === 200) {
+          console.log(data);
+          resetInputs();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Please turn you vpn on :)))");
+      });
+  }
 }
-
 
 function resetInputs() {
   nameInput.value = "";
