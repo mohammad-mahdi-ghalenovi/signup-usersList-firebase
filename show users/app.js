72282d3ff9cbd79a9d315f8968448e3b37260b84
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-06-07 00:54:13 +0430

    get and create users from fire base handled

--- show users/app.js
+++ show users/app.js
@@ -1,3 +1,4 @@
+const usersContainerElem = document.querySelector(".users-container");
 let allUsers;
 
 function getDatas() {
@@ -6,7 +7,26 @@ function getDatas() {
     .then((data) => {
       allUsers = data;
 
-      console.log(allUsers);
+      allUsers = Object.entries(allUsers);
+
+      allUsers.forEach((item) => {
+        usersContainerElem.insertAdjacentHTML(
+          "beforeend",
+          `
+        <div class="user">
+        <div class="user__img"></div>
+        <div class="user__infos">
+          <div class="user__name">${item[1].name} - ${item[1].lname}</div>
+          <div class="user__password">Your password : ${item[1].password}</div>
+        </div>
+        <div class="user__buttons">
+          <button class="user__delete">Delete</button>
+          <button class="user__edit">Edit</button>
+        </div>
+      </div>
+        `
+        );
+      });
     });
 }
 
