f014a28cb7b8d208a371da4017ec97c388ea635d
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-06-07 01:10:59 +0430

    deleting a user succesfully handled

--- show users/app.js
+++ show users/app.js
@@ -1,5 +1,7 @@
 const usersContainerElem = document.querySelector(".users-container");
+const deleteModalElem = document.querySelector(".delete-modal");
 let allUsers;
+let userID = null;
 
 function getDatas() {
   fetch("https://login-page1-1ccde-default-rtdb.firebaseio.com/user.json")
@@ -8,6 +10,8 @@ function getDatas() {
       allUsers = data;
 
       allUsers = Object.entries(allUsers);
+
+      usersContainerElem.innerHTML = "";
 
       allUsers.forEach((item) => {
         usersContainerElem.insertAdjacentHTML(
@@ -20,7 +24,7 @@ function getDatas() {
           <div class="user__password">Your password : ${item[1].password}</div>
         </div>
         <div class="user__buttons">
-          <button class="user__delete">Delete</button>
+          <button class="user__delete" onclick="showDeleteModal('${item[0]}')">Delete</button>
           <button class="user__edit">Edit</button>
         </div>
       </div>
@@ -28,6 +32,29 @@ function getDatas() {
         );
       });
     });
+}
+
+function showDeleteModal(id) {
+  userID = id;
+  deleteModalElem.classList.add("visible");
+}
+
+function closeModal() {
+  deleteModalElem.classList.remove("visible");
+}
+
+async function deleteUser() {
+  await fetch(
+    `https://login-page1-1ccde-default-rtdb.firebaseio.com/user/${userID}.json`,
+    {
+      method: "DELETE",
+    }
+  )
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err));
+
+  getDatas();
+  closeModal();
 }
 
 window.addEventListener("load", () => {
